e23f8181dd44922335393df46e0eb946c97e9fbe
annabush092 <annabush092@gmail.com>
2017-08-30 18:21:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  attr_accessor
+  attr_reader :name, :roster
+  attr_writer
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    self.roster[grade] = [] if !self.roster[grade]
+    self.roster[grade] << name
+  end
+
+  def grade(num)
+    self.roster[num]
+  end
+
+  #sort students alphabetically within grades
+  def sort
+    self.roster.each do |grade, students|
+      students.sort!
+    end
+    self.roster
+  end
+end
